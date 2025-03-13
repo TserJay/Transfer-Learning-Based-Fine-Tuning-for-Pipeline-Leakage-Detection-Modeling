1cff8d341a6d6952e764d9460f834916dd71d940
TserJay <1341494281@qq.com>
2025-03-13 16:47:48 +0800

    updata

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-from datasets.CWRU import CWRU
-from datasets.CWRUFFT import CWRUFFT
-
-
-from datasets.PU import PU
-from datasets.PUFFT import PUFFT
-
-from datasets.PU_type import PU_type
-from datasets.PUFFT_type import PUFFT_type
-
-from datasets.JNU import JNU
-from datasets.JNUFFT import JNUFFT
 from datasets.leak_signals_v1 import leak_signals_v1
 from datasets.leak_signals_v2 import leak_signals_v2
 from datasets.leak_signals_TL import leak_signals_TL
 from datasets.leak_signals import leak_signals
 from datasets.leak_signals_6 import leak_signals_6
 
-
-from datasets.SEU import Md as SEU
-from datasets.SEUFFT import Md as SEUFFT
-
-
-from datasets.PHM import PHM
-from datasets.PHMFFT import PHMFFT
 
 
 
